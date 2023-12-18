75c8c4401f4837970b1e600e875e3f68881d57c6
Chandu <dulamchandu13@gmail.com>
2023-12-18 21:49:25 +0530

    localStorage added

--- 02-ToDoList/todo.js
+++ 02-ToDoList/todo.js
@@ -13,17 +13,26 @@ function addTask() {
 
     let cross = document.createElement("span");
     cross.innerHTML = "\u00d7";
-    // listElement.classList.add("task");
     listElement.appendChild(cross);
   }
+  saveData();
 }
 let list = document.querySelector(".list");
 list.addEventListener("click", (a) => {
   if (a.target.tagName === "LI") {
     a.target.classList.toggle("done");
+    saveData();
   } else if (a.target.tagName === "SPAN") {
     a.target.parentElement.remove();
+    saveData();
   }
 });
 
-// // addTask();
+function saveData() {
+  localStorage.setItem("data", list.innerHTML);
+}
+
+function updateDate() {
+  list.innerHTML = localStorage.getItem("data");
+}
+updateDate();
